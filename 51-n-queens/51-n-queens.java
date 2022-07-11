3fd1b9e1aa184e78df059786daf2aeb83b777c20
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-07-11 08:26:57 +0530

    Time: 13 ms (21.07%), Space: 46.7 MB (19.47%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<List<String>> res;
-    public void findChess(int r,int n,boolean [][]chess,boolean[]leftDiagonal,boolean[]rightDiagonal,boolean[]column )
+    public void findChess(int r,int n,boolean [][]chess,BitSet leftDiagonal,BitSet rightDiagonal,BitSet column )
     {
             // System.out.println("hello");
         if(r==n)
@@ -26,22 +26,28 @@ class Solution {
         {
             if(isQueenSafe(r,c,leftDiagonal,rightDiagonal,column,n)==true)
             {
-                chess[r][c]=leftDiagonal[r-c+n-1]=rightDiagonal[r+c]=column[c]=true;
+                chess[r][c]=true;
+                leftDiagonal.set(r-c+n-1,true);
+                rightDiagonal.set(r+c,true);
+                column.set(c,true);
                 findChess(r+1,n,chess,leftDiagonal,rightDiagonal,column);
-                chess[r][c]=leftDiagonal[r-c+n-1]=rightDiagonal[r+c]=column[c]=false;
+                chess[r][c]=false;
+                leftDiagonal.set(r-c+n-1,false);
+                rightDiagonal.set(r+c,false);
+                column.set(c,false);
             }
         }
     }
-    public boolean isQueenSafe(int r,int c,boolean[]leftDiagonal,boolean[]rightDiagonal,boolean[]column ,int n)
+    public boolean isQueenSafe(int r,int c,BitSet leftDiagonal,BitSet rightDiagonal,BitSet column ,int n)
     {
-        return ((leftDiagonal[r-c+n-1]==true)||(rightDiagonal[r+c]==true)||(column[c]==true))?false:true;
+        return ((leftDiagonal.get(r-c+n-1)==true)||(rightDiagonal.get(r+c)==true)||(column.get(c)==true))?false:true;
     }
     public List<List<String>> solveNQueens(int n) {
         res=new ArrayList<>();
 boolean [][]chess=new boolean[n][n];
-        boolean []leftDiagonal=new boolean[(2*n)-1];
-        boolean []rightDiagonal=new boolean[(2*n)-1];
-        boolean []column=new boolean[n];
+        BitSet leftDiagonal=new BitSet(2*n-1)   ;
+        BitSet rightDiagonal=new BitSet(2*n-1);
+        BitSet column=new BitSet(n);
         findChess(0,n,chess,leftDiagonal,rightDiagonal,column);
         return res;
     }
