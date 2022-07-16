6c32f678e8998cc84755d4a070336dbad99b03b1
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-07-16 19:37:20 +0530

    Time: 0 ms (100.00%), Space: 59.3 MB (5.99%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -1,29 +1,20 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int low = 1, high = arr.length - 2;
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            
-       int lval=(mid-1>=0)?arr[mid-1]:Integer.MIN_VALUE;
-       int rval=(mid+1<arr.length)?arr[mid+1]:Integer.MIN_VALUE;
-       if(arr[mid]>lval&&arr[mid]>rval)
-       {
-        return mid;
-       }
-       else if(lval>arr[mid]&&arr[mid]>rval)
-       {
-        high=mid-1;
-       }
-       else if(lval<arr[mid]&&rval>arr[mid]){
-        low=mid+1;
-       }
-       else
-       {
-        if(arr[low]<=arr[high])low++;
-        else high--;
-       }
+        int low=0;
+        int high=arr.length-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            int tar=arr[mid];
+            int lval=(mid-1>=0)?arr[mid-1]:Integer.MIN_VALUE;
+            int rval=(mid+1<arr.length)?arr[mid+1]:Integer.MIN_VALUE;
+            if(tar>lval&&tar>rval)
+                return mid;
+            if(lval<tar)
+                low=mid+1;
+            else
+                high=mid-1;
         }
         return -1;
-        
     }
 }
