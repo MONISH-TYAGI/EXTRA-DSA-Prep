4019695ba092548ea3db5ed969966f7383f6c8ca
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-07-16 10:38:51 +0530

    Time: 1 ms (36.15%), Space: 42.9 MB (38.76%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,24 +1,24 @@
 class Solution {
-    public void swap(int []nums1,int a ,int b)
+      public void swap(int []nums1,int a ,int b)
     {
         int temp=nums1[a];
         nums1[a]=nums1[b];
         nums1[b]=temp;
     }
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-for(int i=m;i<m+n;i++)
-    nums1[i]=nums2[i-m];
-        
-for(int i=m;i<m+n;i++)
+        for(int i=m;i<m+n;i++)
+            nums1[i]=nums2[i-m];
+for(double gap=(m+n)/2.0;gap>0;gap=gap/2.0)
 {
-    for(int j=i-1;j>=0;j--)
+    int jStart=(int)Math.ceil(gap);
+    for(int i=0,j=jStart;j<m+n;i++,j++)
     {
-        if(nums1[j]>nums1[j+1])
+        if(nums1[i]>nums1[j])
         {
-            swap(nums1,j,j+1);
+            swap(nums1,i,j);
         }
-        else break;
     }
+    if(jStart==1) break;
 }
     }
 }
