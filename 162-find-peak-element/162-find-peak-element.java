29b1742e323bb6ec759e3974f2a80d11e228446a
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-07-16 20:03:23 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (12.37%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -1,21 +1,19 @@
 class Solution {
     public int findPeakElement(int[] arr) {
-        int low = 0, high = arr.length - 1;
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            
-       long lval=(mid-1>=0)?arr[mid-1]:Long.MIN_VALUE;
-       long rval=(mid+1<arr.length)?arr[mid+1]:Long.MIN_VALUE;
-       if((long)arr[mid]>lval&&(long)arr[mid]>rval)
-       {
-        return mid;
-       }
-if(lval<arr[mid])
-{
-    low=mid+1;
-}else
-{high=mid-1;
-       }
+        int low=0;
+        int high=arr.length-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            int tar=arr[mid];
+            long lval=(mid-1>=0)?arr[mid-1]:Long.MIN_VALUE;
+            long rval=(mid+1<arr.length)?arr[mid+1]:Long.MIN_VALUE;
+            if(tar>lval&&tar>rval)
+                return mid;
+            if(lval<tar)
+                low=mid+1;
+            else
+                high=mid-1;
         }
         return -1;
         
