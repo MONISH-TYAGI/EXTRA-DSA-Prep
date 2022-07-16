e0c86cd8d650ae84001b92e28482415d35b28ab2
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-07-16 21:14:32 +0530

    Time: 0 ms (100.00%), Space: 60.5 MB (49.14%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -1,31 +1,18 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-int low=0;
+        int low=0;
         int high=nums.length-1;
         while(low<=high)
         {
             int mid=low+(high-low)/2;
-            int lval=(mid-1>=0)?nums[mid-1]:-1;
-            int rval=(mid+1<nums.length)?nums[mid+1]:-1;
-            if(nums[mid]!=lval&&nums[mid]!=rval)
-            {
+            int lval=(mid-1>=0)?nums[mid-1]:Integer.MIN_VALUE;
+            int rval=(mid+1<nums.length)?nums[mid+1]:Integer.MIN_VALUE;
+            if(lval!=nums[mid]&&rval!=nums[mid])
                 return nums[mid];
-            }
-            if(nums[mid]==lval)
-            {
-                int  lCount=mid-low-1;
-                if(lCount%2==1)
-                    high=mid-2;
-                else
-                    low=mid+1;
-            }else
-            {
-                int lCount=mid-low;
-                if(lCount%2==1)
-                    high=mid-1;
-                else
-                    low=mid+2;
-            }
+            if((mid%2==1&&nums[mid-1]==nums[mid])||(mid%2==0&&nums[mid+1]==nums[mid]))
+                low=mid+1;
+            else
+                high=mid-1;
         }
         return -1;
     }
