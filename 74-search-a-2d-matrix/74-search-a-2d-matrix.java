ac45bd271c682663f3397f6a1150cd597931d435
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-07-16 14:35:07 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (35.40%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,35 +1,14 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        // Linear Search -> Nested Loops 
-//         int row = matrix.length, col = matrix[0].length;
-//         for(int i=0; i<row; i++){
-//             for(int j=0; j<col; j++){
-//                if(matrix[i][j] == target){
-//                    return true;
-//                }
-//             } 
-//         }
-        
-//         return false;
-        
-        int row = 0, col = matrix[0].length - 1; // Top Right Corner
-        
-        while(row < matrix.length && col >= 0){
-            
-            if(matrix[row][col] == target) 
-                return true;
-            
-            else if(matrix[row][col] < target){
-                // Discard the current row
-                row++;
-            }
-            
-            else {
-                // Discard the current col
+        int row=0;
+        int col=matrix[0].length-1;
+        while(row<matrix.length&&col>=0)
+        {
+            if(matrix[row][col]==target) return true;
+           if(matrix[row][col]<target) row++;
+            else
                 col--;
-            }
         }
-        
         return false;
     }
 }
