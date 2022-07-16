9d9cf37718a155fca4b3d7555f3f17711f78bdd4
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-07-16 10:30:25 +0530

    Time: 3 ms (5.38%), Space: 42.9 MB (38.76%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,26 +1,24 @@
 class Solution {
+    public void swap(int []nums1,int a ,int b)
+    {
+        int temp=nums1[a];
+        nums1[a]=nums1[b];
+        nums1[b]=temp;
+    }
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int p3 = nums1.length - 1;
-        int p1 = m - 1, p2 = n - 1;
+for(int i=m;i<m+n;i++)
+    nums1[i]=nums2[i-m];
         
-        while(p1 >= 0 && p2 >= 0){
-            if(nums1[p1] > nums2[p2]){
-                nums1[p3] = nums1[p1];
-                p1--; p3--;
-            } else {
-                nums1[p3] = nums2[p2];
-                p2--; p3--;
-            }
+for(int i=m;i<m+n;i++)
+{
+    for(int j=i-1;j>=0;j--)
+    {
+        if(nums1[j]>nums1[j+1])
+        {
+            swap(nums1,j,j+1);
         }
-        
-        while(p1 >= 0){
-            nums1[p3] = nums1[p1];
-            p1--; p3--;   
-        }
-        
-        while(p2 >= 0){
-            nums1[p3] = nums2[p2];
-            p2--; p3--;
-        }
+        else break;
+    }
+}
     }
 }
