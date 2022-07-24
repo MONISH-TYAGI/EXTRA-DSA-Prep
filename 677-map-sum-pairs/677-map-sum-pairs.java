c1b756f81e2f6695d4200a3a9789af2ffaf37466
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-07-24 19:39:33 +0530

    Time: 21 ms (43.24%), Space: 42.8 MB (74.69%) - LeetHub

--- 677-map-sum-pairs/677-map-sum-pairs.java
+++ 677-map-sum-pairs/677-map-sum-pairs.java
@@ -1,61 +1,83 @@
 class MapSum {
-    public static class Node{
-        private Node[] links = new Node[26];
-        private int isEnd = 0;
- 
-        public Node get(char ch){
-            return links[ch - 'a'];
-        }
- 
-        public void add(char ch){
- 
-            links[ch - 'a'] =new Node();
-        }
- 
-        public int getEnd(){
-            return isEnd;
-        }
- 
-        public void setEnd(int val){
-            isEnd = val;
-        }
+public static class Node{
+    private Node[]children=new Node[26];
+    private boolean isEnd=false;
+    private int val=0;
+    public int pref=0;
+    public boolean contains(char ch)
+    {
+        return (children[ch-'a']!=null);
     }
- 
-    Node root;
-    public MapSum() {
-        root = new Node();
+    public int getVal(){
+        return val;
     }
-    
-    public void insert(String word, int val) {
-        Node curr = root;
-        for(int i=0; i<word.length(); i++){
-            char ch = word.charAt(i);
-            Node child = curr.get(ch);
-            if(child == null) curr.add(ch);
-            curr = curr.get(ch);
-        }
-        curr.setEnd(val);
+    public Node get(char ch)
+    {
+        return children[ch-'a'];
     }
-    
-    public int dfs(Node curr){
-        if(curr == null) return 0;
-        
-        int sum = curr.getEnd();
-        for(char ch = 'a'; ch <= 'z'; ch++){
-            sum += dfs(curr.get(ch));
-        }
-        
-        return sum;
+    public void set(char ch)
+    {
+        children[ch-'a']=new Node();
     }
-    
-    public int sum(String word) {
-        Node curr = root;
-        for(int i=0; i<word.length(); i++){
-            char ch = word.charAt(i);
-            Node child = curr.get(ch);
-            if(child == null) return 0;
-            curr = curr.get(ch);
-        }
-        return dfs(curr);
+    public void setVal(int val)
+    {
+        this.val=val;
+    }
+    public boolean getEnd()
+    {
+        return isEnd;
+    }
+    public void setEnd(){
+        isEnd=true;
     }
 }
+    Node root;
+    public MapSum() {
+       root=new Node(); 
+    }
+    public int search(String word)
+    {
+        Node curr=root;
+        for(int i=0;i<word.length();i++)
+        {
+            char ch=word.charAt(i);
+            if(curr.contains(ch)==false)
+                return 0;
+            curr=curr.get(ch);
+        }
+        return curr.getVal();
+    }
+    public void insert(String key, int val) {
+        int oldval=search(key);
+        Node curr=root;
+        for(int i=0;i<key.length();i++)
+        {
+            curr.pref+=(val-oldval);
+            char ch=key.charAt(i);
+            if(curr.contains(ch)==false)
+                curr.set(ch);
+            curr=curr.get(ch);
+        }
+        curr.pref+=(val-oldval);
+        curr.setVal(val);
+    }
+    
+    public int sum(String prefix) {
+        Node curr=root;
+               for(int i=0;i<prefix.length();i++)
+        {
+            char ch=prefix.charAt(i);
+                   if(curr.contains(ch)==false)
+                return 0;
+            curr=curr.get(ch);
+        } 
+        return curr.pref;
+    }
+}
+
+/**
+ * Your MapSum object will be instantiated and called as such:
+ * MapSum obj = new MapSum();
+ * obj.insert(key,val);
+ * int param_2 = obj.sum(prefix);
+ */
