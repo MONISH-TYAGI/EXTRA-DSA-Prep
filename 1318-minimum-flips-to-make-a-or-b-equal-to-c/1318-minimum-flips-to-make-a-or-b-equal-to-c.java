77a8f25befb70a1492f48398a3c3024be08fdbda
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-07-28 18:44:44 +0530

    Time: 0 ms (100.00%), Space: 41 MB (37.07%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -6,15 +6,14 @@ class Solution {
             int athbit=((a&(1<<i))==0)?0:1;
             int bthbit=((b&(1<<i))==0)?0:1;
             int cthbit=((c&(1<<i))==0)?0:1;
-            int currAns=bthbit|athbit;
-            if(athbit==0&&bthbit==0&&cthbit==1)
-                ans++;
+    if(athbit==0&&bthbit==0&&cthbit==1)
+        ans++;
+    else if(athbit==0&&bthbit==1&&cthbit==0)
+        ans++;
+           else if(athbit==1&&bthbit==0&&cthbit==0)
+        ans++;
             else if(athbit==1&&bthbit==1&&cthbit==0)
                 ans=ans+2;
-            else if(athbit==0&&bthbit==1&&cthbit==0)
-                ans++;
-             else if(athbit==1&&bthbit==0&&cthbit==0)
-                ans++;
         }
         return ans;
     }
