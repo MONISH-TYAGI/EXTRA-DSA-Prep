52d9430a935e8214efaac76b748b23d748e6fda6
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-07-28 18:36:43 +0530

    Time: 4 ms (39.09%), Space: 68.1 MB (13.99%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
@@ -1,22 +1,27 @@
 class Solution {
     public int[] xorQueries(int[] arr, int[][] queries) {
-        int n=arr.length;
-    int qs=queries.length;
-    int []prefXor=new int[n];
-    prefXor[0]=arr[0];
-    for(int i=1;i<n;i++)
-    {
-        prefXor[i]=prefXor[i-1]^arr[i];
-    }
-    int []ans=new int[qs];
-    for(int i=0;i<qs;i++)
-    {
-        int l=queries[i][0];
-        int r=queries[i][1];
-        if(l==0) ans[i]=prefXor[r];
-        else ans[i]=prefXor[r]^prefXor[l-1];
-        
-    }
-    return ans;
+      int []prefixArr=new int[arr.length];
+        prefixArr[0]=arr[0];
+        for(int i=1;i<prefixArr.length;i++)
+        {
+            prefixArr[i]=prefixArr[i-1]^arr[i];
+            
+        }
+        int []res=new int[queries.length];
+        for(int i=0;i<queries.length;i++)
+        {
+int left=queries[i][0];
+            int right=queries[i][1];
+            if(left==0)
+            {
+                res[i]=prefixArr[right];
+            }else
+            {
+                res[i]=prefixArr[left-1]^prefixArr[right];
+            }
+                
+                
+        }
+        return res;
     }
 }
