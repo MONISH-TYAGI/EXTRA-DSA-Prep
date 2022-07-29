af8920c5fe57d9a50f9066976e9f7a4f8a4c9cac
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-07-29 14:52:47 +0530

    Time: 5 ms (53.34%), Space: 55.7 MB (11.10%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,30 +1,52 @@
 class Solution {
-    public void printAllPaths(int src,int dest,List<Integer>path,List<List<Integer>> Path,boolean []vis,int[][] graph)
+    public List<List<Integer>> Path;
+    public class Graph{
+        ArrayList<Integer> []adj;
+        Graph(int n)
+        {
+            adj=new ArrayList[n];
+            for(int i=0;i<n;i++)
+                adj[i]=new ArrayList<>();
+        }
+        public void addEdge(int src,int dest)
+        {
+            adj[src].add(dest);
+            
+        }
+    }
+    public void printAllPath(Graph g,ArrayList<Integer> obj,boolean []vis,int src,int dest)
     {
-        if(vis[src]==true)
-            return ;
         if(src==dest)
         {
-            path.add(dest);
-            Path.add(new ArrayList<>(path));
-            path.remove(path.size()-1);
+            vis[src]=true;
+        obj.add(src);
+            Path.add(new ArrayList<>(obj));
+            vis[src]=false;
+        obj.remove(obj.size()-1);
             return ;
         }
+        if(vis[src]==true) return ;
         vis[src]=true;
-        path.add(src);
-        for(int nbr:graph[src])
+        obj.add(src);
+        for(int nbr:g.adj[src])
         {
-            printAllPaths(nbr,dest,path,Path,vis,graph);
+            printAllPath(g,obj,vis,nbr,dest);
         }
         vis[src]=false;
-        path.remove(path.size()-1);
-        
+        obj.remove(obj.size()-1);
     }
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        List<Integer> path=new ArrayList<>();
-        List<List<Integer>> Path=new ArrayList<>();
+        Path=new ArrayList<>();
+    Graph g=new Graph(graph.length);
+       for(int i=0;i<graph.length;i++)
+       {
+           for(int nbr:graph[i])
+           {
+               g.adj[i].add(nbr);
+           }
+       }
         boolean []vis=new boolean[graph.length];
-         printAllPaths(0,graph.length-1,path,Path,vis,graph);
+    printAllPath(g,new ArrayList<>(),vis,0,graph.length-1);
         return Path;
     }
 }
