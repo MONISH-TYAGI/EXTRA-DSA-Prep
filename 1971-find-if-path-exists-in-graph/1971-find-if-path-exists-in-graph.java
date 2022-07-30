6648a650beb7685c39dfd95086b8869d4ff3407d
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-07-30 13:40:44 +0530

    Time: 131 ms (70.74%), Space: 190.2 MB (66.06%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,40 +1,45 @@
 class Solution {
-    class Graph{
-        public ArrayList<Integer>[]adj;
-Graph(int n)
-{
-    adj=new ArrayList[n];
-    for(int i=0;i<n;i++)
-    {
-        adj[i]=new ArrayList<>();
+    class Graph {
+    public ArrayList<Integer>[] adj;
 
-    }
-}
-        public void addEdge(int src,int dest)
-        {
-            adj[src].add(dest);
-            adj[dest].add(src);
+    Graph(int n) {
+        adj = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
         }
     }
-    public boolean DFS(int src,int dest,boolean []vis,Graph g)
-    {
-        if(src==dest) return true;
-        if(vis[src]==true) return false;
-        vis[src]=true;
-        for(int nbr:g.adj[src])
-        {
-            if(DFS(nbr,dest,vis,g)==true)
-                return true;
-        }
-        return false;
+            public void addEdge(int src, int dest) {
+        adj[src].add(dest);
+        adj[dest].add(src);
     }
+    }
+
+
+        public boolean BFS(boolean []vis,Graph g,int src,int dest)
+        {
+            Queue<Integer> q=new ArrayDeque<>();
+            q.add(src);
+            
+            while(q.size()>0)
+            {
+              int curr=q.remove();
+                if(curr==dest) return true;
+                if(vis[curr]==true) continue;
+                vis[curr]=true;
+                for(int nbr:g.adj[curr])
+                {
+                    q.add(nbr);
+                }
+            }
+            return false;
+        }
     public boolean validPath(int n, int[][] edges, int src, int dest) {
-       Graph g=new Graph(n);
+        Graph g=new Graph(n);
         for(int i=0;i<edges.length;i++)
         {
             g.addEdge(edges[i][0],edges[i][1]);
         }
         boolean []vis=new boolean[n];
-        return DFS(src,dest,vis,g);
+        return BFS(vis,g,src,dest);
     }
 }
