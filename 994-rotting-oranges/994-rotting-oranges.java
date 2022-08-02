fb85f786c888d9cc2ffa12ce05d099ff7c72fd9a
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-08-02 16:53:07 +0530

    Time: 4 ms (47.49%), Space: 43.7 MB (11.83%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -1,57 +1,53 @@
-class Pair{
-    int row, col, dist;
-    
-    Pair(int row, int col, int dist){
-        this.row = row;
-        this.col = col;
-        this.dist = dist;
-    }
-}
 class Solution {
-    int[][] calls = {{+1, 0}, {-1, 0}, {0, +1}, {0, -1}};
-    
+    class Pair{
+int row;
+    int col;
+        int time;
+    Pair(int row,int col,int time)
+    {
+        this.row=row;
+        this.col=col;
+        this.time=time;
+    }}
+    int [][]dir={{0,1},{1,0},{-1,0},{0,-1}};
     public int orangesRotting(int[][] grid) {
-        Queue<Pair> q = new LinkedList<>();
-        for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid[0].length; j++){
-                if(grid[i][j] == 2){
-                    q.add(new Pair(i, j, 0));
-                }
+        Queue<Pair>q=new ArrayDeque<>();
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[0].length;j++)
+            { 
+                if(grid[i][j]==2)
+                q.add(new Pair(i,j,0));
             }
         }
-        
-        int min = 0;
-        while(q.size() > 0){
-            Pair front = q.remove();
-            
-            if(grid[front.row][front.col] == 3) continue; // Already Visited
-            grid[front.row][front.col] = 3; // Visited Mark
-            min = Math.max(min, front.dist);
-            
-            for(int i=0; i<4; i++){
-                int nr = front.row + calls[i][0];
-                int nc = front.col + calls[i][1];
-                
-                // Out of Matrix
-                if(nr < 0 || nc < 0 || nr >= grid.length || nc >= grid[0].length)
+        int min=0;
+        while(q.size()>0)
+        {
+            Pair front=q.remove();
+            if( grid[front.row][front.col]==3) continue;
+            min=Math.max(min,front.time);
+            grid[front.row][front.col]=3;
+            for(int i=0;i<4;i++)
+            {
+                int nr=front.row+dir[i][0];
+                int nc=front.col+dir[i][1];
+                if(nr<0||nc<0||nr>=grid.length||nc>=grid[0].length) continue;
+                if(grid[nr][nc]==3||grid[nr][nc]==0)
                     continue;
-                
-                // Empty Cell or Already Rotten
-                if(grid[nr][nc] == 0 || grid[nr][nc] == 3)
-                    continue;
-                
-                q.add(new Pair(nr, nc, front.dist + 1));
+                q.add(new Pair(nr,nc,front.time+1));
             }
         }
-        
-        for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid[0].length; j++){
-                // System.out.print(grid[i][j] + " ");
-                if(grid[i][j] == 1) return -1; // Not Possible to rot all oranges
+                for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[0].length;j++)
+            { 
+                // System.out.print(grid[i][j]+" ");
+                
+                if(grid[i][j]==1)
+return -1;
             }
-            // System.out.println();
+                    // System.out.println();
         }
-        
         return min;
     }
 }
