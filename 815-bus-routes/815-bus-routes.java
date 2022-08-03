0832132487a257d3b8d865400108b3e71c3e495b
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-08-03 19:24:03 +0530

    Time: 340 ms (10.19%), Space: 245.9 MB (5.01%) - LeetHub

--- 815-bus-routes/815-bus-routes.java
+++ 815-bus-routes/815-bus-routes.java
@@ -1,64 +1,61 @@
 class Solution {
-    public static class Pair implements Comparable<Pair>{
-        int Stop; // NODE
-        int Color; // COLOR (EDGE)
-        int Changes;
-        
-        Pair(int Stop, int Color, int Changes){
-            this.Stop = Stop;
-            this.Color = Color;
-            this.Changes = Changes;
+    class Pair implements Comparable<Pair>{
+        int node;
+        int stops;
+        int color;
+        Pair (int node,int stops,int color)
+        {
+            this.node=node;
+            this.stops=stops;
+            this.color=color;
         }
-        
-        public int compareTo(Pair other){
-            return this.Changes - other.Changes;
+           public int compareTo(Pair other){
+            return this.stops - other.stops;
         }
     }
     public int numBusesToDestination(int[][] routes, int source, int target) {
-        if(source == target) return 0;
-        
-        int n = 0;
-        for(int[] route: routes)
-            for(int stop: route)
-                n = Math.max(n, stop);
-        
-        ArrayList<Pair>[] adj = new ArrayList[n + 1];
-        for(int i=0; i<=n; i++) adj[i] = new ArrayList<>();
-        
-        for(int i=0; i<routes.length; i++){
-            for(int j=0; j<routes[i].length; j++){
-                int src = routes[i][j];
-                int dest = routes[i][(j + 1) % routes[i].length];
-                
-                adj[src].add(new Pair(dest, i, 0));
-                adj[dest].add(new Pair(src, i, 0));
+        int n=0;
+      for(int []route:routes)
+      {
+          for(int nbr:route)
+          {
+              n=Math.max(n,nbr);
+          }
+      }
+        ArrayList<Pair>[]adj=new ArrayList[n+1];
+        for(int i=0;i<=n;i++)
+            adj[i]=new ArrayList<>();
+        for(int i=0;i<routes.length;i++)
+        {
+      for(int j=0;j<routes[i].length;j++)
+      {
+       int src=routes[i][j];
+          int dest=routes[i][(j+1)%routes[i].length];
+          adj[src].add(new Pair(dest,0,i));
+            adj[dest].add(new Pair(src,0,i));
+      }
+            }
+      PriorityQueue<Pair> pq=new PriorityQueue<>();
+pq.add(new Pair(source,0,routes.length)) ;
+    int [][]vis=new int[n+1][routes.length+1];
+        for(int i=0;i<=n;i++)
+        {
+            Arrays.fill(vis[i],-1);
+        }
+        while(pq.size()>0)
+        {
+            Pair front =pq.remove();
+            if(front.node==target)return front.stops;
+            if(vis[front.node][front.color]!=-1)continue;
+            vis[front.node][front.color]=front.stops;
+            for(Pair nbr:adj[front.node])
+            {
+                if(front.color!=nbr.color)
+               pq.add(new Pair(nbr.node,front.stops+1,nbr.color));
+                else
+                    pq.add(new Pair(nbr.node,front.stops,nbr.color));
             }
         }
-        
-        PriorityQueue<Pair> q = new PriorityQueue<>();
-        q.add(new Pair(source, routes.length, 0));
-        
-        int[][] vis = new int[n + 1][routes.length + 1];
-        for(int i=0; i<=n; i++)
-            for(int j=0; j<=routes.length; j++)
-             vis[i][j] = -1;
-        
-        while(q.size() > 0){
-            Pair top = q.remove();
-            if(top.Stop == target) return top.Changes;
-            
-            if(vis[top.Stop][top.Color] != -1) continue;
-            vis[top.Stop][top.Color] = top.Changes;
-            
-            for(Pair nbr: adj[top.Stop]){
-                if(top.Color == nbr.Color){
-                    q.add(new Pair(nbr.Stop, nbr.Color, top.Changes));
-                } else {
-                    q.add(new Pair(nbr.Stop, nbr.Color, top.Changes + 1));
-                }
-            }
-        }
-        
         return -1;
     }
 }
