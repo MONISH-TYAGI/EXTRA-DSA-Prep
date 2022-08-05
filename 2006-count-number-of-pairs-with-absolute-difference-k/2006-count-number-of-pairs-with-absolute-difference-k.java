5e5e63c3a7c72bff01220c2673c054f501f15c74
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-08-05 15:02:34 +0530

    Time: 6 ms (83.20%), Space: 44.6 MB (17.50%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.java
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.java
@@ -1,22 +1,15 @@
 class Solution {
     public int countKDifference(int[] nums, int k) {
         HashMap<Integer, Integer> freq = new HashMap<>();
+        int count=0;
         for (int i = 0; i < nums.length; i++) {
+            if(freq.containsKey(nums[i]-k))
+            count+=freq.get(nums[i]-k);
+            if(freq.containsKey(nums[i]+k))
+            count+=freq.get(nums[i]+k);
             freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
         }
 
-        int count = 0;
-        for (Integer key : freq.keySet()) {
-            int freq1 = freq.get(key);
-
-            if (k == 0) {
-                count = count + 0;
-            } else {
-                int freq2 = freq.getOrDefault(k + key, 0);
-                count = count + freq1 * freq2;
-            }
-        }
-
-        return count;
+return count;
     }
 }
