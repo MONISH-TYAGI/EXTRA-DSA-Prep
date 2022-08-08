bd6123f868dbd9adbec98d2b5d4494b1aa4fb083
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-08-09 00:07:23 +0530

    Time: 169 ms (74.19%), Space: 246.4 MB (83.31%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -1,57 +1,51 @@
 class Solution {
-    // List of Bridges should be returned
-    public int[] disc;
-    public int[] low;
-    public int time = 0;
-    public List<List<Integer>> bridges;
-
-    public void DFS(int src, int parent, ArrayList<Integer>[] adj) {
-        disc[src] = low[src] = time;
-        time++;
-
-        for (Integer nbr : adj[src]) {
-
-            if (nbr == parent)
-                continue;
-
-            if (disc[nbr] >= 0) {
-                // Back Edge (Already Visited Neighbour)
-                low[src] = Math.min(low[src], disc[nbr]);
-            }
-
-            else {
-                // Unvisited Neighbour
-                DFS(nbr, src, adj);
-
-                if (low[nbr] > disc[src]) {
-                    // Neighbour cannot visit the already visited component ignoring the edge
-                    List<Integer> bridge = new ArrayList<>();
-                    bridge.add(src);
-                    bridge.add(nbr);
-                    bridges.add(bridge);
+    List<List<Integer>> obj;
+    int []parent;
+    int []disc;
+    int []low;
+    int count=-1;
+    public void DFS(int src,  ArrayList<Integer>[] adj)
+    {
+        count++;
+      low[src]=disc[src]=count;
+        for(int nbr:adj[src])
+        {
+            if(parent[src]==nbr) continue;
+            if(disc[nbr]==-1)
+            {
+                parent[nbr]=src;
+                DFS(nbr,adj);
+                if(low[nbr]>disc[src])
+                {
+                    List<Integer> ans=new ArrayList<>();
+ans.add(src);ans.add(nbr);
+                    obj.add(ans);
                 }
-
-                low[src] = Math.min(low[src], low[nbr]);
-            }
+                low[src]=Math.min(low[src],low[nbr]);
+                }
+        
+        else
+        {
+           low[src]=Math.min(low[src],disc[nbr]);   
+        }
         }
     }
-
-    @SuppressWarnings("unchecked")
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
-        disc = new int[n];
-        low = new int[n];
-        Arrays.fill(disc, -1); // Discovery is -1 => node is unvisited
-        bridges = new ArrayList<>();
-
-        ArrayList<Integer>[] adj = new ArrayList[n];
-        for (int i = 0; i < n; i++)
-            adj[i] = new ArrayList<>();
-        for (List<Integer> edge : connections) {
+        parent=new int[n];
+        disc=new int[n];
+        low=new int[n];
+        obj=new ArrayList<>();
+        Arrays.fill(disc,-1);
+        ArrayList<Integer>[] adj=new ArrayList[n];
+        for(int i=0;i<n;i++)
+            adj[i]=new ArrayList<>();
+        for(List<Integer> edge:connections)
+        {
             adj[edge.get(0)].add(edge.get(1));
             adj[edge.get(1)].add(edge.get(0));
         }
-
-        DFS(0, -1, adj);
-        return bridges;
+        DFS(0,adj);
+        return obj;
+        
     }
 }
