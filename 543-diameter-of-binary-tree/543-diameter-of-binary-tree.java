330231ee832e49295af03b1590cd84418c723319
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-08-09 21:37:15 +0530

    Time: 1 ms (65.86%), Space: 43.1 MB (70.62%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -14,28 +14,18 @@
  * }
  */
 class Solution {
- public static class Pair{
-        int height;
-        int diameter; // Diameter is of entire subtree
+    public int globalDia=0;
+    public int diameter(TreeNode root)
+    {
+        if(root==null) return 0;
+        int lh=diameter(root.left);
+        int rh=diameter(root.right);
+        globalDia=Math.max(globalDia,lh+rh+1);
+        return Math.max(lh,rh)+1;
     }
-    
-    public Pair diameter(TreeNode root){
-        if(root == null) return new Pair();
-        
-        Pair left = diameter(root.left);
-        Pair right = diameter(root.right);
-        
-        Pair curr = new Pair();
-        curr.height = Math.max(left.height, right.height) + 1;
-        curr.diameter = left.height + right.height + 1;
-        curr.diameter = Math.max(curr.diameter, Math.max(left.diameter, right.diameter));
-        return curr;
-    }
-    
-    public int diameterOfBinaryTree(TreeNode root){
-        if(root == null) return 0;
-        // Diameter in Terms of Edges = Diamater in Terms of Nodes - 1
-        return diameter(root).diameter - 1;
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root==null) return 0;
+        diameter(root);
+        return globalDia-1;
     }
 }
-
