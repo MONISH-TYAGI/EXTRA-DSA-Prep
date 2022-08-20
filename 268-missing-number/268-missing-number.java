cb7040eeb62d036f3919ada8dc80d06e87422bb1
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-08-20 13:24:54 +0530

    Time: 1 ms (78.53%), Space: 50.8 MB (75.13%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,19 +1,19 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
-        
-        // Visited Each Element Present in Array
-        for(int i=0; i<nums.length; i++){
-            int val = nums[i] % (n + 1);
-            if(val < n) nums[val] += (n + 1);
-        }
-        
-        // Find Missing Element
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] < n + 1) return i;
-        }
-        
-        return n; 
-        // If all boxes are visited, then n itself is the answer
+        int n=nums.length;
+        for(int i=0;i<nums.length;i++)
+        {
+            int idx=nums[i]%(n+1);
+            if(idx<n)
+            nums[idx]=nums[idx]+(n+1);
+        }   
+        for(int i=0;i<nums.length;i++)
+        {
+        // System.out.print(nums[i]+" ");
+            if(nums[i]<(n+1))
+                return i;
+        } 
+        return n;
+
     }
 }
