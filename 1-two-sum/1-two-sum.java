26f7dafdff23d270e786d946ea515c56e6834004
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-08-24 12:59:22 +0530

    Time: 2 ms (94.94%), Space: 45.1 MB (56.49%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        //System.out.println("hello");
     HashMap<Integer,Integer> hm=new HashMap<>();
         for(int i=0;i<nums.length;i++)
         {
