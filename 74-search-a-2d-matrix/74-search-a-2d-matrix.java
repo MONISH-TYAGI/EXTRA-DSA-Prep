6b7d46d38a5ea34a734d3fd89a201b0474ce4fa4
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-09-02 19:39:56 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (75.30%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,13 +1,36 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int row=0;
-        int col=matrix[0].length-1;
-        while(row<matrix.length&&col>=0)
+      public static int find(int[][]matrix,int target)
+    {
+                int left=0;
+        int right=matrix.length-1;
+        int lastIdx=matrix[0].length-1;
+        while(left<=right)
         {
-            if(matrix[row][col]==target) return true;
-           if(matrix[row][col]<target) row++;
+            int mid=left+(right-left)/2;
+            if(matrix[mid][0]<=target&&target<=matrix[mid][lastIdx]) return mid;
+            if(matrix[mid][0]<target)
+            left=mid+1;
             else
-                col--;
+            right=mid-1;
+        }
+       return -1; 
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+                int Row=find(matrix,target);
+        // System.out.println(Row);
+if(Row==-1) return false;
+        if(Row<matrix.length&&matrix[Row][0]<=target&&matrix[Row][matrix[0].length-1]>=target){
+            // System.out.println("hello");
+        int left=0;
+        int right=matrix[0].length-1;
+        while(left<=right)
+        {
+            int mid=left+((right-left)/2);
+            // System.out.println("left "+left+" right "+right+" mid "+mid+" val "+matrix[Row][mid]);
+            if(matrix[Row][mid]==target) return true;
+            else if(matrix[Row][mid]<target) left=mid+1;
+            else right=mid-1;
+        }
         }
         return false;
     }
