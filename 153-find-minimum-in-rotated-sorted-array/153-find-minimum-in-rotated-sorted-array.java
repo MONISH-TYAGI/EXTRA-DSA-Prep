dca444dbd0ff52aa8f7dbdfdb478ea9cf0b56658
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-09-02 13:22:51 +0530

    Time: 1 ms (44.36%), Space: 42.5 MB (47.74%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -1,56 +1,22 @@
 class Solution {
-    public boolean isMin(int mid,int []nums)
-    {
-        if(mid>0&&mid<nums.length-1)
-        {
-            if(nums[mid-1]>nums[mid]&&nums[mid]<nums[mid+1])
-                return true;
-        }
-        if(mid==0) return false;
-        if(mid==nums.length-1)
-        {
-            if(nums[mid-1]>nums[mid])
-                return true;
-        }
-        return false;
-    }
-        public boolean isMax(int mid,int []nums)
-    {
-        if(mid>0&&mid<nums.length-1)
-        {
-            if(nums[mid-1]<nums[mid]&&nums[mid]>nums[mid+1])
-                return true;
-        }
-        if(mid==nums.length-1) return false;
-        if(mid==0)
-        {
-            if(nums[mid+1]<nums[mid])
-                return true;
-        }
-        return false;
-    }
-    
-    public int findMin(int[] nums) {
-        int left = 0, right = nums.length - 1;
-        if(nums.length==1)
-            return nums[0];
-        if(nums[0]<nums[right])
-            return nums[0];
+    public int findMin(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        
         while(left <= right){
             int mid = left + (right - left) / 2;
-        
- if(isMin(mid,nums)==true)
-    return nums[mid];
-            else if(isMax(mid,nums)==true)
-    return nums[mid+1];
-            else if(nums[mid] >= nums[left]){
+            
+            int lVal = (mid == 0) ? Integer.MAX_VALUE : arr[mid - 1];
+            int rVal = (mid == arr.length - 1) ? Integer.MAX_VALUE : arr[mid + 1];
+                
+            if(arr[mid] < lVal && arr[mid] < rVal){
+                return arr[mid];
+            } else if(arr[mid] > arr[right]){
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
         
-        // Corner Case: Sorted Array without Rotations
-        return nums[left];
+        return arr[0];
     }
 }
