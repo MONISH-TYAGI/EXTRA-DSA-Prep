48c3608726eea8584cd3218545fc39d0a627e4ed
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-09-03 16:58:37 +0530

    Time: 25 ms (7.15%), Space: 54.6 MB (17.83%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,53 @@
+class Solution {
+            public static boolean canAllocate(int []arr,int mid,int k)
+    {
+        int currPages=0;
+        int count=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(currPages+arr[i]<=mid)
+            {
+                currPages+=arr[i];
+            }
+            else
+            {
+                currPages=arr[i];
+                count++;
+            }
+            if(count>=k) return false;
+        }
+        return true;
+    }
+    public static int findHigh(int []arr)
+    {
+        int sum=0;
+        for(int a:arr)
+        sum+=a;
+        return sum;
+    }
+public static int findLow(int []arr)
+{
+    int max=Integer.MIN_VALUE;
+    for(int a:arr)
+    max=Math.max(max,a);
+    return max;
+}
+    public int shipWithinDays(int[] arr, int m) {
+            int left=findLow(arr);
+    // System.out.println(left);
+    int right=findHigh(arr);
+    // System.out.println(right);
+    int ans=right;
+    while(left<=right)
+    {
+        int mid=left+((right-left)/2);
+        if(canAllocate(arr,mid,m)==true)
+        {
+            ans=mid;
+            right=mid-1;
+        }else
+        left=mid+1;
+    }
+    return ans;    
+    }
+}
