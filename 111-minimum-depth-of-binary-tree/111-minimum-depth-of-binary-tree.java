b1106ad1e3b7dae2ae63ea83d84d4b009e56d051
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-09-14 12:38:25 +0530

    Time: 19 ms (11.95%), Space: 90 MB (59.35%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.java
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.java
@@ -14,19 +14,13 @@
  * }
  */
 class Solution {
-    public int helper(TreeNode root)
-    {
-        if(root==null) return 0;
-        if(root.left==null&&root.right==null)
-            return 1;
-        if(root.left==null)
-            return 1+helper(root.right);
-        if(root.right==null)
-            return 1+helper(root.left);
-        
-        return 1+Math.min(helper(root.left),helper(root.right));
-    }
     public int minDepth(TreeNode root) {
-      return helper(root);  
+        if(root==null) return 0;
+        int left=(root.left!=null)?minDepth(root.left):Integer.MAX_VALUE;
+        int right=(root.right!=null)?minDepth(root.right):Integer.MAX_VALUE;
+        if(left==Integer.MAX_VALUE&&right==Integer.MAX_VALUE)
+            return 1;
+        
+        return Math.min(left,right)+1;
     }
 }
