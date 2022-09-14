6b355621cb31dbe53a13624eff68493c2fdedc31
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-09-14 12:52:36 +0530

    Time: 1 ms (99.26%), Space: 44.1 MB (72.08%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -17,14 +17,13 @@ class Solution {
     public int height(TreeNode root)
     {
         if(root==null) return 0;
-        return (1+Math.max(height(root.left),height(root.right)));
+        return Math.max(height(root.left),height(root.right))+1;
     }
     public boolean isBalanced(TreeNode root) {
         if(root==null) return true;
-        int lheight=height(root.left);
-        int rheight=height(root.right);
-        if(lheight-rheight<-1||lheight-rheight>1)
-            return false;
+        int left=height(root.left);
+        int right=height(root.right);
+        if(Math.abs(left-right)>1) return false;
         return isBalanced(root.left)&&isBalanced(root.right);
     }
 }
