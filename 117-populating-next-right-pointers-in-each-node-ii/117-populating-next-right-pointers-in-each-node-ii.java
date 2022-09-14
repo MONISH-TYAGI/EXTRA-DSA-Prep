61fc4b13052ff079e82e2a60bca05ea286259025
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-09-14 23:53:02 +0530

    Time: 1 ms (78.81%), Space: 44.2 MB (82.73%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -23,56 +23,37 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
-        if(root == null) return root;
-        
-        Queue<Node> q = new ArrayDeque<>();
-        q.add(root);
-        
-        while(q.size() > 0){
-            int count = q.size();
-            for(int i=0; i<count; i++){
-                Node curr = q.remove();
-                
-                // Populate Next Right Pointer of Curr
-                if(i < count - 1) curr.next = q.peek();
-                if(curr.left != null) q.add(curr.left);
-                if(curr.right != null) q.add(curr.right);
+        Node curr=root;
+        while(curr!=null)
+        {
+            Node head=null,tail=null;
+            while(curr!=null)
+            {
+                if(curr.left!=null)
+                {
+                    if(head==null)
+                    {
+                        head=tail=curr.left;
+                    }else
+                    {
+                        tail.next=curr.left;
+                        tail=tail.next;
+                    }
+                }if(curr.right!=null)
+                {
+                     if(head==null)
+                    {
+                        head=tail=curr.right;
+                    }else
+                    {
+                        tail.next=curr.right;
+                        tail=tail.next;
+                    }
+                }
+                curr=curr.next;
             }
+curr=head;
         }
-        
         return root;
     }
-    
-//     public Node connect(Node root){
-//         Node curr = root;
-//         while(curr != null){
-//             Node head = null, tail = null;
-            
-//             // Travel on Current Level and Construct Next Level
-//             while(curr != null){
-//                 if(curr.left != null){
-//                     if(head == null)
-//                         head = tail = curr.left;
-//                     else {
-//                         tail.next = curr.left;
-//                         tail = tail.next;
-//                     }
-//                 }
-//                 if(curr.right != null){
-//                     if(head == null)
-//                         head = tail = curr.right;
-//                     else {
-//                         tail.next = curr.right;
-//                         tail = tail.next;
-//                     }
-//                 }
-                
-//                 curr = curr.next;
-//             }
-            
-//             curr = head; // Update current as Head of Next Level
-//         }
-        
-//         return root;
-//     }
 }
