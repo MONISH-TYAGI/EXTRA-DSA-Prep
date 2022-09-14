36b478bdbbaa267707b0bcd84512c79afe6bb552
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-09-14 19:32:36 +0530

    Time: 7 ms (67.98%), Space: 45.3 MB (24.17%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -1,24 +1,42 @@
-class Solution{
-    HashMap<Long, Integer> freq = new HashMap<>();
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int helper(TreeNode root,int targetSum,long sum,HashMap<Long,Integer>hm)
+    {
+        if(root==null) return 0;
+        sum+=root.val;
+       
+
+        int count=0;
+  if(hm.containsKey(sum-targetSum)==true&&hm.get(sum-targetSum)>0)
+            count=hm.get(sum-targetSum);
+                hm.put(sum,hm.getOrDefault(sum,0)+1);
+        count+=helper(root.left,targetSum,sum,hm);
+        count+=helper(root.right,targetSum,sum,hm);
+
     
-    public int helper(TreeNode root, Long targetSum, Long prefSum){
-        if(root == null) return 0;
-        
-        prefSum += root.val;
-        int count = freq.getOrDefault(prefSum - targetSum, 0);
-        freq.put(prefSum, freq.getOrDefault(prefSum, 0) + 1);
-        
-        count += helper(root.left, targetSum, prefSum);
-        count += helper(root.right, targetSum, prefSum);
-        
-        freq.put(prefSum, freq.getOrDefault(prefSum, 0) - 1); // Backtrack
-        
+        hm.put(sum,hm.getOrDefault(sum,0)-1);
+            // sum-=root.val;
         return count;
     }
-    
-    public int pathSum(TreeNode root, int targetSum){
-        if(root == null) return 0;
-        freq.put(0l, 1);
-        return helper(root, 1l*targetSum, 0l);
+    public int pathSum(TreeNode root, int targetSum) {
+        if(root==null) return 0;
+        HashMap<Long,Integer>freq=new HashMap<>();
+        freq.put(0l,1);
+        return helper(root,targetSum,0l,freq);
+        
     }
 }
