762e163f77976e6229e1ff771bc6b1672a450b0d
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-09-15 19:01:00 +0530

    Time: 2 ms (99.08%), Space: 41.5 MB (99.81%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,19 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public TreeNode buildTree(int[] preorder, HashMap<Integer, Integer> inorder, int inL, int inR, int preIdx){
-        if(inL > inR) return null;
-        
-        TreeNode curr = new TreeNode(preorder[preIdx]);
-        int inIdx = inorder.get(preorder[preIdx]);
-        
-        curr.left = buildTree(preorder, inorder, inL, inIdx - 1, preIdx + 1);
-        curr.right = buildTree(preorder, inorder, inIdx + 1, inR, preIdx + 1 + (inIdx - inL));
-        return curr;
+
+    public TreeNode helper(int []preorder,int []inorder,int left,int right,HashMap<Integer,Integer> hm,int preIdx)
+    {
+        if(left>right) return null;
+        TreeNode node=new TreeNode(preorder[preIdx]);
+        if(left==right)
+        {
+            node.left=null;
+            node.right=null;
+            return node;
+        }
+        int inIdx=hm.get(preorder[preIdx]);
+        node.left=helper(preorder,inorder,left,inIdx-1,hm,preIdx+1);
+        node.right=helper(preorder,inorder,inIdx+1,right,hm,preIdx+(inIdx-left)+1);
+        return node;
     }
-    
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        for(int i=0; i<inorder.length; i++) hm.put(inorder[i], i);
+        HashMap<Integer,Integer> hm=new HashMap<>();
+
+        for(int i=0;i<inorder.length;i++)
+        {
+            hm.put(inorder[i],i);
+        }
         
-        return buildTree(preorder, hm, 0, inorder.length - 1, 0);
+TreeNode ans= helper(preorder,inorder,0,preorder.length-1,hm,0);
+        return ans;
+        // TreeNode curr=ans;
+        // while(curr!=null);
+        // {
+        //     System.out.println(curr.val);
+        //     curr=curr.right;
+        // }
+        // return ans;
     }
 }
