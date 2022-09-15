a709d8d37759f6e9da4541c0ff00dd34cbe6daa9
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-09-15 16:13:57 +0530

    Time: 0 ms (100.00%), Space: 49.8 MB (52.54%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
@@ -1,30 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public int leftMostdepth(TreeNode root){
-        int ans = 0;
-        while(root != null){
+    public int height(TreeNode root)
+    {
+        int ans=0;
+        TreeNode left=root;
+        while(left!=null)
+        {
+            left=left.left;
             ans++;
-            root = root.left;
         }
         return ans;
     }
-    public int rightMostdepth(TreeNode root){
-        int ans = 0;
-        while(root != null){
+        public int height2(TreeNode root)
+    {
+        int ans=0;
+        TreeNode right=root;
+        while(right!=null)
+        {
+            right=right.right;
             ans++;
-            root = root.right;
         }
         return ans;
     }
-    
     public int countNodes(TreeNode root) {
-        if(root == null) return 0;
-        
-        // Is Tree Perfect
-        int left = leftMostdepth(root); // O(H)
-        int right = rightMostdepth(root); // O(H)
-        
-        // No of nodes in perfect tree of height h is 2 ^ h - 1
-        if(left == right) return ((1 << left) - 1);
-        return 1 + countNodes(root.left) + countNodes(root.right);
+        if(root==null) return 0;
+        int left=height(root);
+        int right=height2(root);
+        if(left==right) return (1<<left)-1;
+        return 1+countNodes(root.left)+countNodes(root.right);
     }
 }
