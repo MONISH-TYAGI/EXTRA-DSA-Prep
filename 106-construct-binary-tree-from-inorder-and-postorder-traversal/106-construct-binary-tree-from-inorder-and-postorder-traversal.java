56166a8ee551453b4aed503cb8330a6a115fdc0b
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-09-15 19:33:48 +0530

    Time: 5 ms (32.91%), Space: 45.1 MB (13.43%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -1,19 +1,24 @@
 class Solution {
-    public TreeNode buildTree(int[] postorder, HashMap<Integer, Integer> inorder, int inL, int inR, int postIdx){
-        if(inL > inR) return null;
+    public TreeNode buildTree(int[] postorder, HashMap<Integer, Integer> inorder, int inL, int inR, int preIdx){
+ if(inL > inR) return null;
         
-        TreeNode curr = new TreeNode(postorder[postIdx]);
-        int inIdx = inorder.get(postorder[postIdx]);
+        TreeNode curr = new TreeNode(postorder[preIdx]);
+        int inIdx = inorder.get(postorder[preIdx]);
         
-        curr.left = buildTree(postorder, inorder, inL, inIdx - 1, postIdx - 1 - (inR - inIdx));
-        curr.right = buildTree(postorder, inorder, inIdx + 1, inR, postIdx - 1);
+        curr.left = buildTree(postorder, inorder, inL, inIdx - 1, preIdx -1-(inR-inIdx));
+        curr.right = buildTree(postorder, inorder, inIdx + 1, inR, preIdx -1);
         return curr;
+
     }
     
     public TreeNode buildTree(int[] inorder, int[] postorder) {
+       
+
         HashMap<Integer, Integer> hm = new HashMap<>();
         for(int i=0; i<inorder.length; i++) hm.put(inorder[i], i);
         
-        return buildTree(postorder, hm, 0, inorder.length - 1, postorder.length - 1);
+        return buildTree(postorder, hm, 0, inorder.length - 1, postorder.length-1);
+
+
     }
 }
