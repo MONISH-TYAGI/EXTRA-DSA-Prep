e2a1985e41d2d2e52efef05615642304b4ade198
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-09-19 21:15:13 +0530

    Time: 4 ms (67.52%), Space: 44.8 MB (25.69%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,38 +14,21 @@
  * }
  */
 class Solution {
-
-    public TreeNode helper(int []preorder,int []inorder,int left,int right,HashMap<Integer,Integer> hm,int preIdx)
+    public TreeNode helper(int []preorder,HashMap<Integer,Integer>hm,int inLeft,int inRight,int preLeft,int preRight)
     {
-        if(left>right) return null;
-        TreeNode node=new TreeNode(preorder[preIdx]);
-        if(left==right)
-        {
-            node.left=null;
-            node.right=null;
+        if(inLeft>inRight) return null;
+        TreeNode node=new TreeNode(preorder[preLeft]);
+        int idx=hm.get(preorder[preLeft]);
+        node.left=helper(preorder,hm,inLeft,idx-1,preLeft+1,preLeft+(idx-inLeft));
+            node.right=helper(preorder,hm,idx+1,inRight,preLeft+(idx-inLeft)+1,preRight);
             return node;
-        }
-        int inIdx=hm.get(preorder[preIdx]);
-        node.left=helper(preorder,inorder,left,inIdx-1,hm,preIdx+1);
-        node.right=helper(preorder,inorder,inIdx+1,right,hm,preIdx+(inIdx-left)+1);
-        return node;
     }
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         HashMap<Integer,Integer> hm=new HashMap<>();
-
         for(int i=0;i<inorder.length;i++)
         {
             hm.put(inorder[i],i);
         }
-        
-TreeNode ans= helper(preorder,inorder,0,preorder.length-1,hm,0);
-        return ans;
-        // TreeNode curr=ans;
-        // while(curr!=null);
-        // {
-        //     System.out.println(curr.val);
-        //     curr=curr.right;
-        // }
-        // return ans;
+        return helper(preorder,hm,0,inorder.length-1,0,preorder.length-1);
     }
 }
