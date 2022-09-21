9e194032f7f83291c0a0404baf7ac53b9c2d0a2c
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-09-21 09:00:26 +0530

    Time: 1 ms (80.97%), Space: 43.4 MB (53.93%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.java
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -1,32 +1,37 @@
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public static enum State {
-        CAMERA, NEED, COVERED;
-    }
-    
-    int cameras = 0;
-    
-    public State dfs(TreeNode root){
-        if(root == null) return State.COVERED;
-        
-        State left = dfs(root.left);
-        State right = dfs(root.right);
-        
-        if(left == State.NEED || right == State.NEED){
-            cameras++;
-            return State.CAMERA;
+    int camera=0;
+    public String DFS(TreeNode root)
+    {
+        if(root==null) return "covered";
+        String left=DFS(root.left);
+        String right=DFS(root.right);
+        if(left=="need"||right=="need")
+        {
+            camera++;
+            return "camera";
         }
-        
-        if(left == State.CAMERA || right == State.CAMERA){
-            return State.COVERED;
-        }
-        
-        return State.NEED;
+        if(left=="camera"||right=="camera")
+        return "covered";
+        return "need";
     }
-        
     public int minCameraCover(TreeNode root) {
-        State ans = dfs(root);
-        if(ans == State.NEED) cameras++;
-        return cameras;
+        String ans=DFS(root);
+        if(ans=="need") camera++;
+        return camera;
     }
 }
