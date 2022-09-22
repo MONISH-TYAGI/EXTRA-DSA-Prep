f26281e83f0eb28199169703d1425971fc9014cb
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-09-22 20:04:18 +0530

    Time: 1 ms (80.79%), Space: 43 MB (32.12%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -1,21 +1,55 @@
 class Solution {
-    TreeNode prev = null;
-
+    
+class Pair{
+    TreeNode head;
+    TreeNode tail;
+    Pair()
+    {
+        
+    }
+    Pair(TreeNode x,TreeNode y)
+    {
+        this.head=x;
+        this.tail=y;
+    }
+}
     public void flatten(TreeNode root) {
-        if(root == null){
-            return;
+       if(root!=null)
+           helper(root);
+    }
+    public Pair helper(TreeNode root)
+    {
+        if(root.left!=null&&root.right!=null)
+        {
+         Pair lp=helper(root.left);
+            Pair rp=helper(root.right);
+            Pair mp=new Pair();
+            mp.head=root;
+            mp.head.left=null;
+            mp.head.right=lp.head;
+            lp.tail.right=rp.head;
+            mp.tail=rp.tail;
+            return mp;
+        }else if(root.left!=null)
+        {
+         Pair lp=helper(root.left);
+            Pair mp=new Pair();
+            mp.head=root;
+            mp.head.left=null;
+            mp.head.right=lp.head;
+            mp.tail=lp.tail;
+            return mp;
+        }else if(root.right!=null)
+        {
+            Pair rp=helper(root.right);
+            Pair mp=new Pair();
+            mp.head=root;
+            mp.head.left=null;
+            mp.head.right=rp.head;
+            mp.tail=rp.tail;
+                return mp;
         }
-
-        TreeNode ol = root.left;
-        TreeNode or = root.right;
-
-        if(prev != null){
-            prev.left = null;
-            prev.right = root;
-        }
-        prev = root;
-
-        flatten(ol);
-        flatten(or);
+        else
+            return new Pair(root,root);
     }
 }
