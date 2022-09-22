d152b41648495d0a43330283b20dae07decfd73b
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-09-23 00:21:42 +0530

    Time: 5 ms (89.72%), Space: 47 MB (45.76%) - LeetHub

--- 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.java
+++ 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.java
@@ -14,33 +14,25 @@
  * }
  */
 class Solution {
-    int i = 0;
+    int i=0;
     public TreeNode recoverFromPreorder(String traversal) {
-        return helper(traversal, 0);   
+        return helper(traversal,0);
     }
-    
-    public TreeNode helper(String str, int depth){
-        int j = 0;
-        while(i + j < str.length() && str.charAt(i + j) == '-'){
+    public TreeNode helper(String traversal,int depth)
+    {
+        int j=0;
+        while(i+j<traversal.length()&&traversal.charAt(i+j)=='-')
             j++;
-        }
-        
-        if(j != depth){
-            return null;
-        }
-        
-        int k = 0;
-        while(i + j + k < str.length() && str.charAt(i + j + k) != '-'){
+        if(j!=depth) return null;
+        int k=0;
+        while(i+j+k<traversal.length()&&traversal.charAt(i+j+k)!='-')
             k++;
-        }
-        
-        int val = Integer.parseInt(str.substring(i + j, i + j + k));
-        i = i + j + k;
-        
-        TreeNode node = new TreeNode(val);
-        node.left = helper(str, depth + 1);
-        node.right = helper(str, depth + 1);
-        
+        int val=Integer.parseInt(traversal.substring(i+j,i+j+k));
+        i=i+j+k;
+        TreeNode node=new TreeNode(val);
+        node.left=helper(traversal,depth+1);
+        node.right=helper(traversal,depth+1);
         return node;
     }
+    
 }
