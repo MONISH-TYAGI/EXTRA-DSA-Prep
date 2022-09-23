52e6c5b47a163c1db67c67c3eaca668aececbe72
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-09-23 10:15:49 +0530

    Time: 66 ms (25.43%), Space: 122.7 MB (10.41%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.java
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.java
@@ -1,63 +1,70 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+    public ArrayList<TreeNode> getPath(TreeNode root,int val)
+    {
+        if(root==null) return new ArrayList<>();
+        if(root.val==val)
+        {
+            ArrayList<TreeNode> obj=new ArrayList<>();
+            obj.add(root);
+            return obj;
+        }
+        ArrayList<TreeNode> left=getPath(root.left,val);
+        if(left.size()>0)
+        {
+            left.add(root);
+            return left;
+        }
+        ArrayList<TreeNode> right=getPath(root.right,val);
+                if(right.size()>0)
+        {
+            right.add(root);
+            return right;
+        }
+        return new ArrayList<>();
+    }
     public String getDirections(TreeNode root, int startValue, int destValue) {
-        
-        ArrayList<TreeNode> list1 = getPath(root, startValue);
-        ArrayList<TreeNode> list2 = getPath(root, destValue);
-        
-        int i = list1.size() - 1;
-        int j = list2.size() - 1;
-        
-        while(i >= 0 && j >= 0){
-            if(list1.get(i) == list2.get(j)){
+        ArrayList<TreeNode> l1=getPath(root,startValue);
+        ArrayList<TreeNode> l2=getPath(root,destValue);
+        StringBuilder res=new StringBuilder();
+        // System.out.println(l1);
+        //         System.out.println(l2);
+        int i=l1.size()-1,j=l2.size()-1;
+        while(i>=0&&j>=0)
+        {
+            if(l1.get(i).val==l2.get(j).val)
+            {
                 i--;
                 j--;
-            } else {
-                break;
-            }
+            }else break;
         }
-        
         i++;
         j++;
-        
-        StringBuilder ans = new StringBuilder();
-        for(int ii = 0; ii < i; ii++){
-            ans.append("U");
+        for(int k=1;k<=i;k++)
+        {
+            res.append("U");
         }
-        
-        for(int jj = j; jj > 0; jj--){
-            if(list2.get(jj - 1) == list2.get(jj).left){
-                ans.append("L");
-            } else {
-                ans.append("R");
-            }
+        for(int k=j;k>0;k--)
+        {
+            if(l2.get(k).right==l2.get(k-1))
+                res.append("R");
+            else if(l2.get(k).left==l2.get(k-1))
+                res.append("L");
         }
-        
-        return ans.toString();
-    }
-    
-    public ArrayList<TreeNode> getPath(TreeNode node, int val){
-        if(node == null){
-            return new ArrayList<>();
-        }
-        
-        if(node.val == val){
-            ArrayList<TreeNode> ret = new ArrayList<>();
-            ret.add(node);
-            return ret;
-        } else {
-            ArrayList<TreeNode> left = getPath(node.left, val);
-            if(left.size() > 0){
-                left.add(node);
-                return left;
-            } else {
-                ArrayList<TreeNode> right = getPath(node.right, val);
-                if(right.size() > 0){
-                    right.add(node);
-                    return right;
-                } else {
-                    return new ArrayList<>();
-                }
-            }
-        }
+        return res.toString();
     }
 }
