c781b864dacf261c0671f00c235c129393838205
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-09-23 10:23:38 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (69.79%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.java
@@ -1,23 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    int sum = 0;
-    
-    public int sumNumbers(TreeNode root) {
-        sum = 0;
-        helper(root, 0);
-        return sum;
+    int sum=0;
+    public void helper(TreeNode root,int val)
+    {
+        
+        if(root==null) return ;
+        
+        helper(root.left,val*10+root.val);
+        helper(root.right,val*10+root.val);
+        if(root.left==null&&root.right==null)
+        {
+            // System.out.println(val);
+            sum+=val*10+root.val;
+        }
     }
-    
-    public void helper(TreeNode node, int num){
-        if(node == null){
-            return;
-        }
-        
-        helper(node.left, num * 10 + node.val);
-        helper(node.right, num * 10 + node.val);
-        
-        if(node.left == null && node.right == null){
-            num = num * 10 + node.val;
-            sum += num;
-        }
+    public int sumNumbers(TreeNode root) {
+        helper(root,0);
+        return sum;
     }
 }
