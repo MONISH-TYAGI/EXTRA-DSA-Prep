3ee8198f4d7e3f7f3c62f249ab64f8c9e7678722
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-09-23 11:33:41 +0530

    Time: 4 ms (30.40%), Space: 43.8 MB (43.62%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
@@ -1,43 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    class Pair {
+    class Pair{
         long min;
         long max;
+        Pair(long x,long y)
+        {
+            min=x;
+            max=y;
+        }
     }
-    
-    long maxW = 0;
+    long maxW=1l;
+    public void helper(int depth,long idx,HashMap<Integer,Pair> hm,TreeNode root)
+    {
+        if(root==null) return ;
+        if(hm.containsKey(depth))
+        {
+            Pair curr=hm.get(depth);
+            curr.min=Math.min(curr.min,idx);
+                curr.max=Math.max(curr.max,idx);
+            maxW=Math.max(maxW,(curr.max-curr.min)+1l);
+            hm.put(depth,curr);
+        }else
+        {
+         Pair curr=new Pair(idx,idx);
+            hm.put(depth,curr);
+        }
+        helper(depth+1,2*idx,hm,root.left);
+        helper(depth+1,2*idx+1,hm,root.right);
+        
+        
+    }
     public int widthOfBinaryTree(TreeNode root) {
-        HashMap<Integer, Pair> map = new HashMap<>();
-        maxW = 0;
-        helper(root, 1, 1, map);
-        return (int)maxW;
+        if(root==null) return 0;
+        HashMap<Integer,Pair> hm=new HashMap<>();
+        helper(0,1l,hm,root);
+            return (int)maxW;
     }
-    
-    public void helper(TreeNode node, int level, long idx, HashMap<Integer, Pair> map){
-        if(node == null){
-            return;
-        }
-        
-        helper(node.left, level + 1, 2 * idx, map);
-        helper(node.right, level + 1, 2 * idx + 1, map);
-        
-        Pair p = null;
-        
-        if(map.containsKey(level)){
-            p = map.get(level);
-            p.max = idx;
-        } else {
-            p = new Pair();
-            p.min = idx;
-            p.max = idx;
-            map.put(level, p);
-        }
-        
-        long width = p.max - p.min + 1;
-        if(width > maxW){
-            maxW = width;
-        }
-    }
-    
-    
-    
 }
