a78994c1de6b673c0bc1ee0fbb3d74b7b1302c78
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-10-18 10:39:08 +0530

    Time: 2 ms (42.76%), Space: 41.4 MB (26.60%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
@@ -1,19 +1,21 @@
 class Solution {
     public int kthFactor(int n, int k) {
-        for(int i=1;i<Math.sqrt(n);i++)
+        List<Integer>first=new ArrayList<>();
+        List<Integer> second=new ArrayList<>();
+        for(int i=1;i*i<=n;i++)
         {
             if(n%i==0)
-                k--;
-            if(k==0)
-                return i;
+            {
+first.add(i);
+                if(n/i!=i)
+                    second.add(n/i);
+            }
         }
-        for(int i=(int)Math.sqrt(n);i>=1;i--)
-        {
-            if(n%i==0)
-                k--;
-            if(k==0)
-                return n/i;
-        }
-        return -1;
+        Collections.reverse(second);
+        List<Integer>factors=new ArrayList<>(first);
+        factors.addAll(second);
+        if(k>factors.size())
+            return -1;
+        return factors.get(k-1);
     }
 }
