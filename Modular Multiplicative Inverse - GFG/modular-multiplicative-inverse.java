029a4c1da870d36c5efeb9c4bb9710a1f8e15b0b
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2022-10-19 09:25:58 +0530

    Added solution - LeetHub

---
+++ Modular Multiplicative Inverse - GFG/modular-multiplicative-inverse.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class Main {
+	public static void main (String[] args) {
+	    
+	    //taking input using Scanner class
+		Scanner sc=new Scanner(System.in);
+		
+		//taking testcases
+		int T=sc.nextInt();
+		
+		while(T-->0)
+		{
+		    Solution  obj=new Solution ();
+		    int a,m;
+		    
+		      
+            //taking input a and m
+		    a=sc.nextInt();
+		    m=sc.nextInt();
+		    
+		    
+		  
+            //calling function modInverse()
+		    System.out.println(obj.modInverse(a,m));
+		   
+		    
+		}
+		
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    
+  public int modInverse(int a, int m)
+    {
+      //Your code here
+      int c=-1;
+
+      for(int i=1;i<m;i++){
+
+          if((i*a)%m==1)
+
+          c= i;
+
+      }
+
+      return c;
+
+
+    }
+
+}
