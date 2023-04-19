97794a77e70f5395dca593d9af386d6d9213e6b7
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2023-04-19 18:26:44 +0530

    Time: 1220 ms (86.10%), Space: 0B (100.00%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,9 @@
+delete from Person
+where id not in (
+                select id 
+                from
+                    (select min(id) as Id,Email
+                    from Person
+                    group by Email
+                    order by Id) i
+                )
