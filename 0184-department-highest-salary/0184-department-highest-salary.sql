9f4f9e3d88f71079801ebffaf644ad21e267c066
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2023-04-19 18:27:34 +0530

    Time: 1189 ms (74.91%), Space: 0B (100.00%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,3 @@
+SELECT D.name AS Department, E.name AS Employee,E.salary AS Salary
+from Employee E,Department D
+where D.id=E.departmentId and E.salary=(SELECT max(salary) from Employee E where E.departmentId=D.id);
