399122c135bac13d0e934218d24e464dcff5a92f
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2023-04-26 18:27:36 +0530

    Time: 2 ms (6.60%), Space: 40.4 MB (8.61%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,16 +1,16 @@
 class Solution {
-    public int addDigits(int nums) {
-      while(nums>9)
-      {
-          String res=nums+"";
-          int sum=0;
-          for(int i=0;i<res.length();i++)
-          {
-              int dig=res.charAt(i)-'0';
-              sum+=dig;
-          }
-          nums=sum;
-      }
-        return nums;
+    public int addDigits(int num) {
+        int sum=num;
+        String word=num+"";
+        while(word.length()>1){
+            sum=0;
+        for(int i=0;i<word.length();i++)
+        {
+            sum+=word.charAt(i)-'0';
+            
+        }
+            word=sum+"";
+        }
+        return sum;
     }
 }
