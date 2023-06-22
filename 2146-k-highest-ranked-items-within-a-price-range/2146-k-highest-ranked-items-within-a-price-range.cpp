e100290875cd34251073c16e5b20d2cceb0e143e
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2023-06-23 00:43:02 +0530

    Time: 945 ms (47.12%), Space: 219.2 MB (42.41%) - LeetHub

--- 2146-k-highest-ranked-items-within-a-price-range/2146-k-highest-ranked-items-within-a-price-range.cpp
+++ 2146-k-highest-ranked-items-within-a-price-range/2146-k-highest-ranked-items-within-a-price-range.cpp
@@ -1,42 +1,46 @@
 class Solution {
 public:
     vector<vector<int>> highestRankedKItems(vector<vector<int>>& grid, vector<int>& price, vector<int>& start, int k) {
-       int m=grid.size(),n=grid[0].size();
-        vector<vector<int>> ans,res;
-        vector<vector<bool>> seen(m,vector<bool>(n,false));
+   int n=grid.size(),m=grid[0].size();
+        vector<vector<int>> res;
+vector<vector<bool>> v(n,vector<bool> (m,false));
         queue<pair<int,int>> q;
         q.push({start[0],start[1]});
-        vector<vector<int>> dirs={{-1,0},{0,-1},{0,1},{1,0}};
         int dist=0;
+         vector<vector<int>>dirs={{1,0},{0,1},{-1,0},{0,-1}};
         while(!q.empty())
         {
             int size=q.size();
-            while(size--)
+            while(size-->0)
             {
-                auto p=q.front();
+                pair curr=q.front();
                 q.pop();
-                if(seen[p.first][p.second]) continue;
-                if(grid[p.first][p.second]==0) continue;
-                seen[p.first][p.second]=true;
-                if(grid[p.first][p.second]!=1)
+                if(v[curr.first][curr.second]==true) continue;
+                v[curr.first][curr.second]=true;
+                int val=grid[curr.first][curr.second];
+                if(val!=1){
+                if(val>=price[0]&&val<=price[1])
                 {
-                    int val=grid[p.first][p.second];
-                    if(val>=price[0]&&val<=price[1])
-                        res.push_back({dist,val,p.first,p.second});
-                    
+                    res.push_back({dist,val,curr.first,curr.second});
                 }
-                for(auto &dir:dirs)
+                }
+                for(int i=0;i<dirs.size();i++)
                 {
-                    int row=p.first+dir[0],col=dir[1]+p.second;
-                    if(row>=0&&row<m&&col>=0&&col<n)
-                        q.push({row,col});
+                    int newRow=curr.first+dirs[i][0],newCol=curr.second+dirs[i][1];
+                    if(newRow>=0&&newRow<n&&newCol>=0&&newCol<m&&grid[newRow][newCol]!=0)
+                        q.push({newRow,newCol});
                 }
+                
             }
             dist++;
+            
         }
         sort(res.begin(),res.end());
-        for(int i=0;i<min(k,(int)res.size());i++)
-            ans.push_back({res[i][2],res[i][3]});
-        return ans;
+        vector<vector<int>> ans;
+        for(int i=0;i<min(k,int(res.size()));i++)
+            {
+                ans.push_back({res[i][2],res[i][3]});
+            }
+            return ans;
     }
 };
