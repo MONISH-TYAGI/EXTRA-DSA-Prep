ecc356795e3b37af8c9e71d751d9bbba36d36cfb
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2023-08-07 10:44:10 +0530

    Time: 8 ms (35.75%), Space: 16 MB (5.88%) - LeetHub

---
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.cpp
@@ -0,0 +1,21 @@
+#define ll long long
+#define mod 1000000007
+class Solution {
+public:
+    
+    ll solve(int currPos,int usedSong,int goal,int N,int k,vector<vector<ll>> &dp)
+    {
+        if(currPos==goal)
+            return (usedSong==N);
+        if(dp[usedSong][currPos]!=-1 ) return dp[usedSong][currPos];
+    ll usedSongPlay=((solve(currPos+1,usedSong,goal,N,k,dp)%mod)*(max(0,usedSong-k)%mod))%mod;
+        ll newSongPlay=((solve(currPos+1,usedSong+1,goal,N,k,dp)%mod)*((N-usedSong)%mod))%mod;
+        ll total=(usedSongPlay+newSongPlay)%mod;
+        return dp[usedSong][currPos]=total;
+    }
+    int numMusicPlaylists(int n, int goal, int k) {
+ 
+        vector<vector<ll >> dp(101,vector<ll>(101,-1));
+        return (int)solve(0,0,goal,n,k,dp);
+    }
+};
