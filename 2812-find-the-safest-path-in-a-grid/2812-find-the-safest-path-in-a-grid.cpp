4b35e1082167d38278c8850daaf56f4040db7b97
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2023-08-13 13:16:22 +0530

    Time: 465 ms (80.22%), Space: 136.6 MB (69.76%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
@@ -1,112 +1,84 @@
-// class Solution {
-// public:
-//     int dx[4]={1,-1,0,0};
-//     int dy[4]={0,0,1,-1};
-//     int maximumSafenessFactor(vector<vector<int>>& grid) {
-//         int r=grid.size();
-//         int c=grid[0].size();
-//         queue<pair<int,int>> q;
-//         for(int i=0;i<r;i++)
-//         {
-//             for(int j=0;j<c;j++)
-//             {
-//                 if(grid[i][j]==1)
-//                     q.push({i,j});
-//             }
-//         }
-//         if(grid[0][0]==1||grid[n-1][n-1]==1) return 0; 
-//         while(q.size()>0)
-//         {
-//             int f=q.front().first;
-//             int s=q.front().second;
-//             int val=grid[f][s];
-//             q.pop();
-//             for(int x=0;x<4;x++)
-//             {
-//                 int i=f+dx[x];
-//                 int j=s+dy[x];
-//                 if(i>=0&&j>=0&&i<r&&j<c&&grid[i][j]==0)
-//                 {
-//                     grid[i][j]=val+1;
-//                     q.push({i,j});
-//                 }
-//             }
-//         }
-//     }
-// };
-
 class Solution {
 public:
     int dx[4]={1,-1,0,0};
     int dy[4]={0,0,1,-1};
-	// .......................DFS.............................
-   bool dfs(vector<vector<int>> &grid,int r,int c,int i,int j,int val,vector<vector<bool>> &visit){
-            if(i < 0 || j < 0 ||i >= r || j >= c || visit[i][j])
-                    return false ;
+     bool isExist(int r,int c,int n,vector<vector<int>>&grid,int val,vector<vector<bool>>&box)
+    {
+        if(r==n-1&&c==n-1)return grid[r][c] >= val;
+         box[r][c]=true;
+        for(int i=0;i<4;i++)
+        {
+            int nr=r+dx[i];
+            int nc=c+dy[i];
+        if(nr>=0&&nr<n&&nc>=0&&nc<n&&grid[nr][nc]>=val&&box[nr][nc]==false)
+        {
+            bool ans=isExist(nr,nc,n,grid,val,box);
+            if(ans==true) return true;
+        }
             
-            if(i == r-1 &&  j == c-1){
-                    return grid[i][j] >= val;
-            }
-            
-            visit[i][j] = true;
-       
-            
-            for(int x = 0;x<4;x++){
-                     int ni = i + dx[x];
-                     int nj = j + dy[x];
-      
-                     if(grid[i][j] >= val && dfs(grid,r,c,ni,nj,val,visit)) return true;
-            }
-            return false;
-   }    
-        
+        }
+         // box[r][c]=false;
+        return false;
+    }
     int maximumSafenessFactor(vector<vector<int>>& grid) {
-            
-           
-            
-            int r =  grid.size();
-            int c = grid[0].size();
-            queue<pair<int,int>> q;
-            for(int i = 0;i<r;i++){
-                    for(int j = 0;j<c;j++){
-                            if(grid[i][j] == 1)
-                                    q.push({i,j});
-                    }
+        int r=grid.size();
+        int c=grid[0].size();
+        int n=grid.size();
+        queue<pair<int,int>> q;
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                if(grid[i][j]==1)
+                    q.push({i,j});
             }
-             if(grid[0][0] == 1 || grid[r-1][c-1] == 1) return 0;
-            //.................................BFS........................//
-            while(!q.empty()){
-                    int f = q.front().first;
-                    int s = q.front().second;
-                    int val = grid[f][s];
-                    q.pop();
-                    
-                    for(int x = 0;x<4;x++){
-                            int i = f + dx[x];
-                            int j = s + dy[x];
-                            if(i >= 0 && j >= 0 && i < r && j < c && grid[i][j] == 0){
-                                    grid[i][j] = val + 1;
-                                    q.push({i,j});
+        }
+        if(grid[0][0]==1||grid[n-1][n-1]==1) return 0; 
+        while(q.size()>0)
+        {
+            int f=q.front().first;
+            int s=q.front().second;
+            int val=grid[f][s];
+            q.pop();
+            for(int x=0;x<4;x++)
+            {
+                int i=f+dx[x];
+                int j=s+dy[x];
+                if(i>=0&&j>=0&&i<r&&j<c&&grid[i][j]==0)
+                {
+                    grid[i][j]=val+1;
+                    q.push({i,j});
+                }
+                
+          else     if(i >= 0 && j >= 0 && i < r && j < c){
+                                    grid[i][j] = min(grid[i][j],val+1);
                             }
-                    }
             }
+        }
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=0;j<n;j++)
+        //         cout<<grid[i][j]<<" ";
+        //     cout<<endl;
+        // }
+        // cout<<endl;
+        int ans=0;
+        int left=0,right=401;
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            vector<vector<bool>> box(n,vector<bool>(n,false));
+            if(isExist(0,0,n,grid,mid,box)&&grid[0][0]>=mid)
+            {
+                 // cout<<left<<" "<<right<<" "<<mid<<endl;
+                ans=max(ans,mid);
+                left=mid+1;
+                
+               
+            }else
+                right=mid-1;
             
-            
-            //..............................BINARY SEARCH...............//
-            int ans = 0;
-            int l = 0 , rr = 500;
-            while(l <= rr){
-            int mid = (l + rr) / 2;        
-            vector<vector<bool>> visit(r,vector<bool>(c,false));
-            if(dfs(grid,r,c,0,0,mid,visit))
-                    ans = max(ans,mid),
-                    l = mid + 1;
-            else
-                    rr = mid - 1;
-            }
-           
-            return ans-1;
-            
-        
+        }
+        return ans-1;
     }
 };
