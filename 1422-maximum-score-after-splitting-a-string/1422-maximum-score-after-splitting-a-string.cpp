6e4e29bf6a2515871d6a31e892d5fdfdb97372a5
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2023-12-23 11:34:00 +0530

    Time: 4 ms (23.01%), Space: 6.6 MB (88.05%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -3,16 +3,6 @@ public:
     int maxScore(string s) {
     int ans=INT_MIN;
         int f=0,sc=0;
-        // bool one=false;
-        // bool zero=false;
-        // for(char ch:s)
-        // {
-        //     if(ch=='1')
-        //         one=true;
-        //     else
-        //         zero=true;
-        // }
-        // if(one==false||zero==false) return s.length()-1;
         for(int i=0;i<s.length()-1;i++)
         {
        if(s[i]=='0')
