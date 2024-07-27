8f3c8366193723a5515ad962e60569463a2bd56a
MONISH-TYAGI <94779549+MONISH-TYAGI@users.noreply.github.com>
2024-07-28 03:14:35 +0530

    Time: 718 ms (62.00%), Space: 326.6 MB (37.51%) - LeetHub

--- 3203-find-minimum-diameter-after-merging-two-trees/3203-find-minimum-diameter-after-merging-two-trees.cpp
+++ 3203-find-minimum-diameter-after-merging-two-trees/3203-find-minimum-diameter-after-merging-two-trees.cpp
@@ -1,51 +1,41 @@
-class Solution
-{
+class Solution {
 public:
-    int solve(vector<vector<int>> &adj, int st, int &farn)
+    vector<vector<int>> makeGraph(vector<vector<int>>&edges)
     {
-        int n = adj.size();
-        farn = st;
-        vector<int> dis(n, -1);
-        helper(adj, st, 0, farn,dis);
-        return dis[farn];
-    }
-
-    void helper(vector<vector<int>> &adj, int node, int distance, int &farn,vector<int> &dis)
-    {
-        dis[node] = distance;
-        if (distance > dis[farn])
-            farn = node;
-        for (auto it : adj[node])
-            if (dis[it] == -1)
-                helper(adj, it, distance + 1, farn,dis);
-    }
-    int minimumDiameterAfterMerge(vector<vector<int>> &nums1, vector<vector<int>> &nums2)
-    {
-        int n = nums1.size();
-        int m = nums2.size();
-        vector<vector<int>> adj1(n + 1);
-        vector<vector<int>> adj2(m + 1);
-        for (auto &it : nums1)
+        int n=edges.size();
+        vector<vector<int>> gr(n+1);
+        for(auto edge : edges)
         {
-            adj1[it[1]].push_back(it[0]);
-            adj1[it[0]].push_back(it[1]);
+            int u=edge[0],v=edge[1];
+            gr[u].push_back(v);
+            gr[v].push_back(u); 
         }
-        for (auto &it : nums2)
+        return gr;
+    }
+    void findFarthest(int node,int par,int dist,vector<vector<int>> &gr,pair<int,int> &res)
+    {
+        if(dist>res.first)
         {
-
-            adj2[it[1]].push_back(it[0]);
-            adj2[it[0]].push_back(it[1]);
+            res={dist,node};
         }
-
-        int far1 = 0;
-        solve(adj1, 0, far1);
-        int diameter1 = solve(adj1, far1, far1);
-
-        int far2 = 0;
-        solve(adj2, 0, far2);
-        int diameter2 = solve(adj2, far2, far2);
-
-        // cout<<diameter1<<" "<<diameter2<<" ";
-        return max({(diameter1 + 1) / 2 + (diameter2 + 1) / 2 + 1, diameter1, diameter2});
+        for(auto child:gr[node])
+        {
+            if(child!=par)
+            {
+                findFarthest(child,node,dist+1,gr,res);
+            }
+        }
+    }
+    int minimumDiameterAfterMerge(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
+        auto gr1=makeGraph(edges1),gr2=makeGraph(edges2);
+        pair<int,int> res={INT_MIN,-1},res2={INT_MIN,-1}; //dest,node
+        findFarthest(0,-1,0,gr1,res);
+        findFarthest(res.second,-1,0,gr1,res2);
+        int diameter1=res2.first;
+               res={INT_MIN,-1};res2={INT_MIN,-1}; //dest,node
+        findFarthest(0,-1,0,gr2,res);
+        findFarthest(res.second,-1,0,gr2,res2);
+        int diameter2=res2.first;
+        return max({diameter1,diameter2,(diameter1+1)/2 +(diameter2+1)/2 +1} );
     }
 };
